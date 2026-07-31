b3c6451de2fbbf20835c42d2370604b40bad5d38
Jefferson Laspinas <its.laspinasjefferson@gmail.com>
2026-07-31 20:52:45 +0800

    Remove all files

--- src/postprocessing/layers.js
+++
@@ -1,9 +0,0 @@
-export const BLOOM_LAYER = 1;
-
-export function enableBloom(object3d) {
-  object3d.traverse((child) => {
-    if (child.isMesh || child.isLineSegments || child.isLine || child.isPoints) {
-      child.layers.enable(BLOOM_LAYER);
-    }
-  });
-}
